16d57a33a2db1879b5446e47b1570b479c0bf0cf
joce <jocelynlegault@gmail.com>
2023-06-29 11:10:37 -0400

    Add examples for many of the possible calls
    Add example for most of the types of calls that can be make with the
    API. This was used to make sure the fixes made to support the cookie-
    based calls are actually working when making several calls in a row.
    
    Also remove broken to get the TWTR symbol as it's no longer traded and
    resulted in errors being generated.

--- examples/main.go
+++ examples/main.go
@@ -2,47 +2,172 @@ package main
 
 import (
 	"fmt"
+	"github.com/piquette/finance-go/chart"
+	"github.com/piquette/finance-go/crypto"
+	"github.com/piquette/finance-go/datetime"
+	"github.com/piquette/finance-go/equity"
+	"github.com/piquette/finance-go/etf"
+	"github.com/piquette/finance-go/forex"
+	"github.com/piquette/finance-go/mutualfund"
 	"github.com/piquette/finance-go/options"
+	"github.com/piquette/finance-go/quote"
 )
 
 // This file lists several usage examples of this library
 // and can be used to verify behavior.
 func main() {
 
-	iter := options.GetStraddle("AAPL")
+	// Basic options example.
+	// --------------------
+	{
+		fmt.Println("Options stradle example\n====================\n")
+		iter := options.GetStraddle("AAPL")
 
-	fmt.Println(iter.Meta())
+		fmt.Println(iter.Meta())
 
-	for iter.Next() {
-		fmt.Println(iter.Straddle().Strike)
-	}
-	if iter.Err() != nil {
-		fmt.Println(iter.Err())
+		for iter.Next() {
+			fmt.Println(iter.Straddle().Strike)
+		}
+		if iter.Err() != nil {
+			fmt.Println(iter.Err())
+		}
+		fmt.Println()
 	}
 
 	// Basic quote example.
 	// --------------------
-	// q, err := quote.Get("SPY")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// } else {
-	// 	fmt.Println(q)
-	// }
+	{
+		fmt.Println("Quote example\n====================\n")
+		q, err := quote.Get("GOOG")
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(q)
+		}
+		fmt.Println()
+	}
 
 	// Basic chart example.
 	// --------------------
-	// params := &chart.Params{
-	// 	Symbol:   "TWTR",
-	// 	Interval: datetime.OneHour,
-	// }
-	// iter := chart.Get(params)
-	//
-	// for iter.Next() {
-	// 	b := iter.Bar()
-	// 	fmt.Println(datetime.FromUnix(b.Timestamp))
-	//
-	// }
-	// if iter.Err() != nil {
-	// 	fmt.Println(iter.Err())
-	// }
+	{
+		fmt.Println("Chart example\n====================\n")
+		params := &chart.Params{
+			Symbol:   "GOOG",
+			Interval: datetime.OneHour,
+		}
+		iter := chart.Get(params)
+
+		for iter.Next() {
+			b := iter.Bar()
+			fmt.Println(datetime.FromUnix(b.Timestamp))
+
+		}
+		if iter.Err() != nil {
+			fmt.Println(iter.Err())
+		}
+		fmt.Println()
+	}
+
+	// Basic crypto example.
+	// --------------------
+	{
+		fmt.Println("Crypto example\n====================\n")
+		q, err := crypto.Get("BTC-USD")
+
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(q)
+		}
+		fmt.Println()
+	}
+
+	// Basic equity example.
+	// --------------------
+	{
+		fmt.Println("Equity example\n====================\n")
+		symbols := []string{"AAPL", "GOOG", "MSFT"}
+		iter := equity.List(symbols)
+
+		if iter.Err() != nil {
+			fmt.Println(iter.Err())
+			fmt.Println()
+		} else {
+			for iter.Next() {
+				q := iter.Equity()
+				fmt.Println(q)
+				fmt.Println()
+			}
+		}
+	}
+
+	// Basic ETF example.
+	// --------------------
+	{
+		fmt.Println("ETF example\n====================\n")
+		q, err := etf.Get("SPY")
+
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(q)
+		}
+		fmt.Println()
+	}
+
+	// Basic forex example.
+	// --------------------
+	{
+		fmt.Println("Forex example\n====================\n")
+		q, err := forex.Get("CADUSD=X")
+
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(q)
+		}
+		fmt.Println()
+	}
+
+	// Basic future example.
+	// --------------------
+	{
+		fmt.Println("Future example\n====================\n")
+		q, err := forex.Get("CL=F")
+
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(q)
+		}
+		fmt.Println()
+	}
+
+	// Basic index example.
+	// --------------------
+	{
+		fmt.Println("Index example\n====================\n")
+		q, err := forex.Get("^DJI")
+
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(q)
+		}
+		fmt.Println()
+	}
+
+	// Basic mutual fund example.
+	// --------------------
+	{
+		fmt.Println("Mutual fund example\n====================\n")
+		q, err := mutualfund.Get("FMAGX")
+
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(q)
+		}
+		fmt.Println()
+	}
 }
